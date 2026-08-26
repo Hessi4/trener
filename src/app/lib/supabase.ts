cc2eed174532fa4db040d293261deda703081964
Mateusz <fg8w7kyvwk@privaterelay.appleid.com>
2026-08-26 14:46:01 +0200

    fix base

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -1,7 +1,7 @@
-// src/lib/supabase.ts
+// src/app/lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://phqazdptgglkbjsqllqn.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_-MVnNlA7cwiXQnyCRc9myQ_nT6nWsEt';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
